1694d3de7b1ed1021eee0fab5d7a2fcf59e6fc94
Alessandro0003 <alessandroquinta3@gmail.com>
2024-07-25 22:19:26 -0300

    🐝 refactor: change logic to export pdf

--- backend/src/controllers/generate-pdf-controller.ts
+++ backend/src/controllers/generate-pdf-controller.ts
@@ -1,20 +1,21 @@
-import { Request, Response } from 'express'
-import { generateServicePDF } from '../services/generate-pdf.service'
+import { Request, Response } from 'express';
+import { generateServicePDF } from '../services/generate-pdf.service';
 
 export const generatePdfController = async (req: Request, res: Response) => {
-  const { url } = req.body
+  const { url } = req.body;
 
   if (!url) {
-    return res.status(400).send('URL is required')
+    return res.status(400).send('URL is required');
   }
 
   try {
-    console.log(`Generating PDF for URL: ${url}`)
-    const pdfBuffer = await generateServicePDF(url)
-    res.setHeader('Content-Type', 'application/pdf')
-    res.send(pdfBuffer)
+    console.log(`Generating PDF for URL: ${url}`);
+    const pdfBuffer = await generateServicePDF(url);
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.send(pdfBuffer);
   } catch (error) {
-    console.error('Error generating PDF:', error.message)
-    res.status(500).send('Failed to generate PDF')
+    console.error('Error generating PDF:', error.message);
+    res.status(500).send('Failed to generate PDF');
   }
-}
+};
--- backend/src/services/generate-pdf.service.ts
+++ backend/src/services/generate-pdf.service.ts
@@ -1,34 +1,33 @@
-import puppeteer from 'puppeteer'
+import puppeteer from 'puppeteer';
 
-export const generateServicePDF = async (
-  url: string,
-): Promise<{ pdf: Buffer; html: string }> => {
+export const generateServicePDF = async (url: string): Promise<Buffer> => {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  })
+  });
 
-  const page = await browser.newPage()
-  await page.goto(url, { waitUntil: 'networkidle0', timeout: 60000 }) // Aumente o timeout para 60 segundos
+  const page = await browser.newPage();
 
-  // Aguarde um tempo adicional usando waitForTimeout
-  await page.setDefaultTimeout(60000)// 10 segundos
+  try {
+    await page.goto(url, { waitUntil: 'networkidle0', timeout: 60000 });
 
-  // Obtenha o conteúdo HTML da página
-  const html = await page.content()
+    // Aumente o tempo de espera para garantir que todos os recursos sejam carregados
+    await page.setDefaultNavigationTimeout(60000);
 
-  // Gere o PDF
-  const pdfBuffer = await page.pdf({
-    format: 'A4',
-    printBackground: true,
-    margin: {
-      top: '10mm',
-      right: '10mm',
-      bottom: '10mm',
-      left: '10mm',
-    },
-  })
+    // Capture o conteúdo completo da página, incluindo imagens, vídeos e scripts
+    const pdfBuffer = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+      margin: {
+        top: '10mm',
+        right: '10mm',
+        bottom: '10mm',
+        left: '10mm',
+      },
+    });
 
-  await browser.close()
-  return { pdf: pdfBuffer, html }
-}
+    return pdfBuffer;
+  } finally {
+    await browser.close();
+  }
+};
